6a7f93469a85dbcc0d7df9b5b14bd771cc6342e2
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2022-11-17 08:51:42 -0500

    Initial Commit

---
+++ InterfaceReproduction/InterfaceReproductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceReproductionApp.swift
+//  InterfaceReproduction
+//
+//  Created by Evelyn Chen on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceReproductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
